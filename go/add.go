877641e1daa94c5b008a0bc8a3bdb2518f06b940
Reneechang17 <minnie90061717@gmail.com>
2026-01-28 22:36:22 -0800

    impl add api (need to fix)

--- go/add.go
+++ go/add.go
@@ -2,6 +2,7 @@ package diccionario
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,9 +17,53 @@ type AddRequest struct {
 func (s *Server) Add(c *gin.Context) {
 	var req AddRequest
 	if err := c.BindJSON(&req); err != nil {
-		c.String(http.StatusBadRequest, err.Error())
+		// should return in JSON with appropriate err info
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
 		return
 	}
 
 	// implement your logic here
+
+	word := strings.TrimSpace(req.Word)
+
+	// validate the input first
+	if !isValid(word) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Word can only contain alphabetic characters"})
+		return
+	}
+
+	// check if it already exists
+	wordlist, err := s.w.GetWords() 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Fail to read wordList"})
+	}
+
+	target := strings.ToLower(word)
+	for _, w := range wordlist {
+		if strings.ToLower(strings.TrimSpace(w)) == target {
+			c.Status(http.StatusConflict)
+			return
+		}
+	}
+
+	if err := s.w.AddWord(word); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Fail to add new word"})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+
+}
+
+func isValid(word string) bool {
+	if word == "" {
+		return false
+	}
+
+	for _, r := range word {
+		if !((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z')) {
+			return false
+		}
+	}
+	return true
 }
